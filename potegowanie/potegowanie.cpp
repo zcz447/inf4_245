a5299d140a4958890b5e47bba250818308d08a8c
zcz447 <zczer447@gmail.com>
2024-11-13 23:07:05 +0100

    poprawa potegowania, uwzgledniono liczby ujemne, zmiennoprzecinkowe oraz urojone + .gitignore

--- potegowanie/potegowanie.cpp
+++ potegowanie/potegowanie.cpp
@@ -1,14 +1,18 @@
 #include <iostream>
-#include <cctype>
-using namespace std;
+#include <cctype> //dla tolower()
+#include <cmath> //dla abs(), floor()
+
+float PotegowanieRekurencyjne(float podstawa, int wykladnikCalkowita);
+float PotegowanieIteracyjne(float podstawa, int wykladnikCalkowita);
+
+using std::cout, std::cin;
 
 
-int PotegowanieRekurencyjne(int podstawa, int wykladnik);
-int PotegowanieIteracyjne(int podstawa, int wykladnik);
 
 int main(){
     
-    int podstawa, wykladnik;
+    float podstawa, wykladnik;
+    float wynik = 1;
     char wybor;
 
     cout << "| Potegowanie |\n\npodaj podstawe: ";
@@ -16,44 +20,105 @@ int main(){
     cout << "podaj wykladnik: ";
         cin >> wykladnik;
 
+// informacja o liczbie ujemnej, zmiana wykladnika na liczbe rzeczywista dodatnia jezeli tak, podzielenie na czesc calkowita i po przecinku (reszta)
+        bool wykladnikUjemny = (wykladnik < 0);
+            if (wykladnikUjemny == true){wykladnik = abs(wykladnik);}
+
+        int wykladnikCalkowita = floor(wykladnik);
+        float wykladnikReszta = wykladnik - wykladnikCalkowita;
+
+
+
+// zakonczenie bez wykonywania zbednych operacji poprzez wylapanie wykladnikow [-1,0,1] oraz podstawy [0,1]
+       if(wykladnik==0){
+
+            cout << "wynik: " << 1;
+            return 0;
+        }
+
+            else if(wykladnik==1){
+
+                if(wykladnikUjemny==false) {cout << "wynik: " << podstawa;}
+                    else{cout << "wynik: " << 1 / podstawa;}
+                return 0;
+            }
+
+            else if (podstawa == 0) {
+
+                cout << "wynik: " << 0;
+                return 0;
+                }
+
+            else if (podstawa == 1){
+
+                cout << "wynik: " << 1;
+                return 0;
+            }
+
+//policzenie PRZYBLIZONEJ reszty poza funkcja
+            if (wykladnikReszta > 0){wynik = wynik + wykladnikReszta * (podstawa -1);}
+
+//sprawdzenie czy ta liczba moze istniec
+    if (podstawa < 0 && wykladnikReszta > 0){
+
+        wynik*=PotegowanieRekurencyjne(podstawa,wykladnikCalkowita);
+
+        if (wykladnikUjemny==false){cout << "brak rozwiązania, wynikiem jest: " << wynik << "i - liczba urojona"; }
+                else{cout << "brak rozwiązania, wynikiem jest: " << 1 / wynik << "i - liczba urojona"; }
+        return 0;
+    }
+
+// zapytanie o wybor rodzaju funkcji
     while(wybor != 'i' && wybor != 'r'){
+
         cout << "\niteracyjne [i]\nrekurencyjne [r]\n";
             cin >> wybor;
         wybor = tolower(wybor);
     }
 
+// wykonanie funkcji w zaleznosci od wyboru
     if (wybor == 'i'){
-        cout << "wynik: " << PotegowanieIteracyjne(podstawa,wykladnik);
+
+        wynik*=PotegowanieIteracyjne(podstawa,wykladnikCalkowita);
+
+        if (wykladnikUjemny==false) {cout << "wynik: " << wynik;}
+            else {cout << "wynik: " << 1 / wynik;}        
     }    
         else{
-            cout << "wynik: " << PotegowanieRekurencyjne(podstawa,wykladnik);
+            wynik*=PotegowanieRekurencyjne(podstawa,wykladnikCalkowita);
+            
+            if (wykladnikUjemny==false) {cout << "wynik: " << wynik;}
+                else {cout << "wynik: " << 1 / wynik;}
         }
 
     return 0;
 };
 
+float PotegowanieIteracyjne(float podstawa, int wykladnikCalkowita){
 
-int PotegowanieIteracyjne(int podstawa, int wykladnik){
-    int wynik = podstawa;
-    if(wykladnik ==1){
-        return podstawa;
-    }
-    else{
-        for (int i=1;i < wykladnik; i++){
-            wynik = wynik * podstawa;
+    float wynik = 1;
+
+    // TO DZIAŁA WRESZCZIE, NAPISALAM WYKLADNIKRESZTA W IF I SZUKALAM BLEDU POL GODZINY, KOCHAM UMIEJETNOSC CZYTANIA ZE ZROZUMIENIEM
+        while (wykladnikCalkowita > 0) {
+
+            if (wykladnikCalkowita % 2 == 1) {
+                wynik *= podstawa;
+                wykladnikCalkowita--;
+            }
+                else{
+                    podstawa *= podstawa;
+                    wykladnikCalkowita = floor(wykladnikCalkowita/ 2);
+                } 
         }
-        return wynik;
-    }
+            return wynik;
 };
-int PotegowanieRekurencyjne(int podstawa, int wykladnik){
 
-    if (wykladnik==0){
-        return 1;
-    }
-        else if (wykladnik % 2 == 0){
-            return PotegowanieRekurencyjne(podstawa*podstawa, wykladnik/2);
-        }
-            else if (wykladnik % 2 == 1) {
-                return podstawa * PotegowanieRekurencyjne(podstawa, wykladnik-1);
-            };
+
+
+float PotegowanieRekurencyjne(float podstawa, int wykladnikCalkowita){
+
+//milo wiedziec ze wybucha bez pierwszej czesci, naprawde milusio
+    if (wykladnikCalkowita==0){return 1;}
+        else if (wykladnikCalkowita % 2 == 0){return PotegowanieRekurencyjne(podstawa*podstawa, wykladnikCalkowita/2);}
+        else if (wykladnikCalkowita % 2 == 1) {return podstawa * PotegowanieRekurencyjne(podstawa, wykladnikCalkowita-1);};
 };
